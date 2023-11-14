0ef07959c1ebfedc62feaba100238ec77276e4e3
karuirujackson <jkjackaruiru641@gmail.com>
2023-11-14 22:05:52 +0300

    Added hash and assign methods

--- HashMap.js
+++ HashMap.js
@@ -4,8 +4,28 @@ class HashMap {
         .fill(null);
     }
 
-    
+    hash(key) {
+      let hashCode = 0;  //Initialize hashCode variable to 0
+      for (let i = 0; i < key.length; i++) {
+        hashCode += hashCode + key.charCodeAt(i); //The hashCode += allows the hashing function to avoid generating duplicate hashCodes if keys have the same characters in different orders, such as bat and tab
+      }
+      return hashCode % this.hashmap.length;
+    }
+
+    assign(key, value) {
+      const arrayIndex = this.hash(key);
+      this.hashmap[arrayIndex] = value;
+    }
 }
   
 module.exports = HashMap;
   
+
+const myHashMap = new HashMap(3);
+console.log(myHashMap.hash('id'));
+console.log(myHashMap.hash('id'));
+
+
+const employees = new HashMap(3);
+employees.assign('34-567', 'Mara');
+console.log(employees.hashmap);
